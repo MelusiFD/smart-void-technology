8183af6eb073bd552e78dfbda937cb9b0f02dffa
Melusi Francis Demadema <melusidemadema@gmail.com>
2023-12-31 12:43:50 +0200

    New Site created

---
+++ assets/js/Index.js
@@ -0,0 +1,18 @@
+document.getElementById("website").onclick = function(){
+    document.location = "webdev.html";
+}
+document.getElementById("desktop").onclick = function(){
+    document.location = "desktop.html";
+}
+document.getElementById("graphics").onclick = function(){
+    document.location = "graphics.html";
+}
+document.getElementById("hosting").onclick = function(){
+    document.location = "hosting.html";
+}
+document.getElementById("project").onclick = function(){
+    document.location = "project.html";
+}
+document.getElementById("android").onclick = function(){
+    document.location = "android.html";
+}
